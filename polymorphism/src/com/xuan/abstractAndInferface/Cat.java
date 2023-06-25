1b7e7bede216d0fa98fb0c97c6dc592590917f41
listen is rain <1986825490@qq.com>
2023-06-25 14:48:58 +0800

    master commit2(polymorphism-abstractAndInferface)

---
+++ polymorphism/src/com/xuan/abstractAndInferface/Cat.java
@@ -0,0 +1,19 @@
+package com.xuan.abstractAndInferface;
+
+/**
+ * @author lirongxuan
+ * @version 1.0
+ * @date 2023/6/25 14:19
+ * @Description: 实现接口里的所有方法。
+ */
+public class Cat implements Pet{
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ polymorphism/src/com/xuan/abstractAndInferface/Pet.java
@@ -0,0 +1,15 @@
+package com.xuan.abstractAndInferface;
+
+/**
+ * @author lirongxuan
+ * @version 1.0
+ * @date 2023/6/25 14:04
+ * @Description: 定义接口
+ */
+public interface Pet {
+
+    void eat();
+
+    void run();
+
+}
